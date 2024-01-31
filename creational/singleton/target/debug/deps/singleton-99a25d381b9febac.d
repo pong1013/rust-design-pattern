2a68c37208f4840139b8af6b62adaff1235c7de1
pong1013 <pong861013@gmail.com>
2024-01-31 07:37:54 +0000

    CH05: Singleton

---
+++ creational/singleton/target/debug/deps/singleton-99a25d381b9febac.d
@@ -0,0 +1,8 @@
+/workspaces/rust/rust-design-pattern/creational/singleton/target/debug/deps/singleton-99a25d381b9febac.rmeta: src/lib.rs src/singleton.rs
+
+/workspaces/rust/rust-design-pattern/creational/singleton/target/debug/deps/libsingleton-99a25d381b9febac.rlib: src/lib.rs src/singleton.rs
+
+/workspaces/rust/rust-design-pattern/creational/singleton/target/debug/deps/singleton-99a25d381b9febac.d: src/lib.rs src/singleton.rs
+
+src/lib.rs:
+src/singleton.rs:
