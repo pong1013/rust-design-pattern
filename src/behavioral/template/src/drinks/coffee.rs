c3d9ec6b7c3c1ca120fdefc40b437416334f151e
pong861013 <pong861013@gmail.com>
2024-03-01 03:13:11 +0000

    CH8: Template

---
+++ src/behavioral/template/src/drinks/coffee.rs
@@ -0,0 +1,13 @@
+use super::CaffeineBeverage;
+
+pub struct Coffee;
+
+impl CaffeineBeverage for Coffee {
+    fn brew(&self) {
+        println!("Dripping Coffee through filter");
+    }
+
+    fn add_condiment(&self) {
+        println!("Adding Sugar and Milk");
+    }
+}
---
+++ src/behavioral/template/src/main.rs
@@ -0,0 +1,12 @@
+use crate::drinks::*;
+use template::*;
+fn main() {
+    let tea = Tea;
+    let coffee = Coffee;
+
+    println!("\nMaking tea...");
+    tea.prepare_recipe();
+
+    println!("\nMaking coffee...");
+    coffee.prepare_recipe();
+}
